689b4e0edab11a73f71a2bf275088a634230ebea
FrancoRosa <km115.franco@gmail.com>
2023-08-25 12:56:29 -0500

    Add array of illegals

--- fm_freq.py
+++ fm_freq.py
@@ -2,9 +2,11 @@
 thr = 0.01    # Nivel de potencia
 width_th = 10 # Ancho de senal
 sdr_gain = 6
+
 print("Threshold:", thr)
 print("Width:", width_th)
 print("Gain:", sdr_gain)
+
 authorized = [
     88.3,
     89.3,
--- scan.py
+++ scan.py
@@ -102,7 +102,7 @@ for i in authorized:
     log = log + check
 
 with open(file, 'a') as f:
-    f.write(log + ", %d, %d\n" % (len(illegal), len(not_found)))
+    f.write(log + ", %d, %d, %s\n" % (len(illegal), len(not_found), str(illegal)))
 
 print("not_found:", not_found)
 print("illegal:", illegal)
